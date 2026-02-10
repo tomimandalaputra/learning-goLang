601cf40a2564e0bfa81703551099783b8a8c403d
tomimandalaputra <tommymandalaputra@gmail.com>
2026-02-10 10:17:34 +0700

    learn how to test confusing code

---
+++ section14/user/greeting_test.go
@@ -0,0 +1,51 @@
+package user
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Greeting(prefix, name string) {
+	fmt.Printf("Hello, %s %s!\n", prefix, name)
+}
+
+func Greeting2(out io.Writer, prefix, name string) {
+	out.Write([]byte(fmt.Sprintf("Hello, %s %s!\n", prefix, name)))
+}
+
+func TestGreeting(t *testing.T) {
+	originalStdout := os.Stdout
+
+	r, w, err := os.Pipe()
+	assert.NoError(t, err)
+
+	os.Stdout = w
+	defer func() {
+		os.Stdout = originalStdout
+	}()
+
+	Greeting("Mr.", "Tomi")
+	w.Close()
+
+	var buf strings.Builder
+	_, err = io.Copy(&buf, r)
+	assert.NoError(t, err)
+
+	want := "Hello, Mr. Tomi!\n"
+	got := buf.String()
+	assert.Equal(t, want, got)
+}
+
+func TestGreeting2(t *testing.T) {
+	var buf strings.Builder
+	Greeting2(&buf, "Mr.", "Tomi")
+
+	want := "Hello, Mr. Tomi!\n"
+	got := buf.String()
+	assert.Equal(t, want, got)
+}
